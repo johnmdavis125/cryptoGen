56ac02e161576a61668ab6d0d500b6bd42dbbbf7
John Davis <johnmdavis125@gmail.com>
2022-02-02 20:27:47 -0500

    clean misc comments

--- script.js
+++ script.js
@@ -116,66 +116,12 @@ const analyzeText = (cleanedUserInput) => {
 //////////////
 // CIPHER
 //////////////
+const alphaMap = {0:'a',1:'b',2:'c',3:'d',4:'e',5:'f',6:'g',7:'h',8:'i',9:'j',10:'k',11:'l',12:'m',13:'n',14:'o',15:'p',16:'q',17:'r',18:'s',19:'t',20:'u',21:'v',22:'w',23:'x',24:'y',25:'z'}
 
-const alphaMap = {
-    0:'a',
-    1:'b',
-    2:'c',
-    3:'d',
-    4:'e',
-    5:'f',
-    6:'g',
-    7:'h',
-    8:'i',
-    9:'j',
-    10:'k',
-    11:'l',
-    12:'m',
-    13:'n',
-    14:'o',
-    15:'p',
-    16:'q',
-    17:'r',
-    18:'s',
-    19:'t',
-    20:'u',
-    21:'v',
-    22:'w',
-    23:'x',
-    24:'y',
-    25:'z'
-}
-const numMap = {
-    a:0,
-    b:1,
-    c:2,
-    d:3,
-    e:4,
-    f:5,
-    g:6,
-    h:7,
-    i:8,
-    j:9,
-    k:10,
-    l:11,
-    m:12,
-    n:13,
-    o:14,
-    p:15,
-    q:16,
-    r:17,
-    s:18,
-    t:19,
-    u:20,
-    v:21,
-    w:22,
-    x:23,
-    y:24,
-    z:25
-}
+const numMap = {a:0,b:1,c:2,d:3,e:4,f:5,g:6,h:7,i:8,j:9,k:10,l:11,m:12,n:13,o:14,p:15,q:16,r:17,s:18,t:19,u:20,v:21,w:22,x:23,y:24,z:25}
+
 let cipherAlpha = []; 
 let cipherNums = []; 
-
 const genRandCipher = () => {
     let randNum = 0; 
     
@@ -243,7 +189,7 @@ const genBoxes = () => {
     let currentWordInLoop = 0; 
     for (let i = 0; i < cryptograph.length; i++){
         // try to incorporate specialCharArr
-        console.log(`this is cryptograph on line 243: ${cryptograph}`); 
+        console.log(`this is cryptograph: ${cryptograph}`); 
         if (specialCharArr[i] === 'cap'){ 
             const letterBox = document.createElement("div"); 
             letterBox.classList.add('letterBox'); 
@@ -271,19 +217,6 @@ const genBoxes = () => {
             currentWordInLoop += 1;
             console.log(`Now on word: ${currentWordInLoop}`);              
         }
-        
-        // don't touch below
-        // if (cryptograph[i] !== ' '){
-        //     const letterBox = document.createElement("div"); 
-        //     letterBox.classList.add('letterBox'); 
-        //     const letter = document.createTextNode(`${cryptograph[i]}`);
-        //     letterBox.appendChild(letter); 
-        //     const theAppropriateWordBox = document.querySelector(`.wordBox-${currentWordInLoop}`); 
-        //     theAppropriateWordBox.appendChild(letterBox); 
-        // } else {
-        //     currentWordInLoop += 1;
-        //     console.log(`Now on word: ${currentWordInLoop}`);  
-        // }
     }
 }
 
@@ -314,8 +247,8 @@ const resetInput = () => {
 }
 
 const handleInput = (userInputText) => {
-    console.log('user clicked submit');
-    console.log(`original str: ${userInputText}`) 
+    // console.log('user clicked submit');
+    // console.log(`original str: ${userInputText}`) 
     cleanInput(userInputText);
     resetInput(); 
     genRandCipher(); 
@@ -337,7 +270,8 @@ submit.addEventListener('click', ()=>{
 
 
 // Re-factor Ideas
-    // Extract out logic for creating DOM elements into a func
+// include accounting for blank submission in cleanInput function!    
+// Extract out logic for creating DOM elements into a func
     // Use recursion for cipherGen func
     // Should only need one charMap for cipher, not two...revisit
     // abbreviated syntax for if|else (ref: u-course)
